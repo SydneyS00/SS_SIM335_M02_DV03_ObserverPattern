00c04127e33de154e14345039930a21df2272484
SydneyS00 <sydschmitz03@outlook.com>
2023-09-23 18:09:29 -0700

    Build and webBuild

---
+++ Observer/Assets/Scripts/ClientObserver.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClientObserver : MonoBehaviour
+{
+    private BikeController _bikeController;
+
+    void Start()
+    {
+        _bikeController = (BikeController)FindObjectOfType(typeof(BikeController));
+    }
+
+    void OnGUI()
+    {
+        if (GUILayout.Button("Damage Bike"))
+            if (_bikeController)
+                _bikeController.TakeDamage(15.0f);
+
+        if (GUILayout.Button("Toggle Turbo"))
+            if (_bikeController)
+                _bikeController.ToggleTurbo(); 
+    }
+}
